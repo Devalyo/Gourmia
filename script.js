a11aa6ad3a320430ad8170380f4f406326b77be2
Daniel de Carvalho <117108120+Devalyo@users.noreply.github.com>
2024-09-16 16:20:56 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
+
+    const whatsAppCircle = document.querySelector('.float');
+    const pecaScreen     = document.querySelector('#peca');
+
     const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
     const sections = navLinks.forEach(link => {
         link.addEventListener('click', function(e) {
@@ -13,14 +17,35 @@ document.addEventListener('DOMContentLoaded', function() {
         const scrollPos = window.scrollY + 100; 
         navLinks.forEach(link => {
             const section = document.querySelector(link.getAttribute('href'));
-            if (section.offsetTop <= scrollPos && section.offsetTop + section.offsetHeight > scrollPos) {
+            if (section.offsetTop <= scrollPos && section.offsetTop + section.offsetHeight > scrollPos) 
+            {
                 link.classList.add('active');
-            } else {
+            } 
+            else 
+            {
                 link.classList.remove('active');
             }
         });
+    
+    }
+
+    function hideWhatsapp()
+    {
+        const windowHeight = window.innerHeight;
+        const pecaRect = pecaScreen.getBoundingClientRect();
+
+        if(pecaRect.top < windowHeight && pecaRect.bottom >= 0)
+        {
+            whatsAppCircle.setAttribute("style", "display: none")
+        }
+        else
+        {
+            whatsAppCircle.setAttribute("style", "display: block")
+        }
+
     }
 
     window.addEventListener('scroll', setActiveLink);
+    window.addEventListener('scroll', hideWhatsapp);
     setActiveLink(); 
 });
